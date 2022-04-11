0390166d98aca69c7e64e14cc79bdffc7d3b021d
WangLin Chung <walnsrully@outlook.com>
2022-04-11 16:23:35 +0800

    feat(pageInfo): 添加分页起始计算静态方法

--- leadnews-common/src/main/java/com/bytedance/leadnews/common/pojo/dto/PageInfo.java
+++ leadnews-common/src/main/java/com/bytedance/leadnews/common/pojo/dto/PageInfo.java
@@ -1,5 +1,6 @@
 package com.bytedance.leadnews.common.pojo.dto;
 
+import com.bytedance.leadnews.common.exception.ParamRequestException;
 import lombok.Data;
 
 import java.util.List;
@@ -38,5 +39,25 @@ public class PageInfo<E> {
         this.list = list;
         this.totalPage = total%size==0?total/size:total/size+1;
         return this;
+    }
+
+    /**
+     * 校验传来的分页参数是否合法
+     * @param page 当前页
+     * @param size 每页大小
+     */
+    public static void checkedPage(Integer page, Integer size) {
+        if (page==null || page<1 || size==null || size<1) {
+            throw new ParamRequestException("参数不合法");
+        }
+    }
+
+    /**
+     * 计算分页起始位置
+     * @param page 当前页
+     * @param size 每页大小
+     */
+    public static Long limit(Integer page, Integer size) {
+        return Long.parseLong(String.valueOf((page-1)*size));
     }
 }
