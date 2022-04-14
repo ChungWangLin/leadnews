23ac0174cca279c6f924701e6fbaff66280f7846
WangLin Chung <walnsrully@outlook.com>
2022-04-14 18:30:29 +0800

    style(paramRequestException): 增加一个空构造

--- leadnews-common/src/main/java/com/bytedance/leadnews/common/exception/ParamRequestException.java
+++ leadnews-common/src/main/java/com/bytedance/leadnews/common/exception/ParamRequestException.java
@@ -1,9 +1,11 @@
 package com.bytedance.leadnews.common.exception;
 
 public class ParamRequestException extends RuntimeException{
-    private  String message;
+    private final String message;
 
-    public ParamRequestException() {}
+    public ParamRequestException() {
+        this.message = "参数不合法";
+    }
 
     public ParamRequestException(String message){
         this.message = message;
